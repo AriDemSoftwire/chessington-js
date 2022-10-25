5ecbe517783a440c60cdbeb137ad552d6faa0b8f
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-25 11:58:35 +0100

    Test for knight completes successfully

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +8,25 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let arrayOfAvailableMoves = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+                let availableSquare = Square.at(i, j); 
+            if (Math.abs(currentSquare.row - availableSquare.row) === 2) {
+                if (Math.abs(currentSquare.col - availableSquare.col) === 1) {
+                    arrayOfAvailableMoves.push(availableSquare);
+                }
+            }
+            if (Math.abs(currentSquare.col - availableSquare.col) === 2) {
+                if (Math.abs(currentSquare.row - availableSquare.row) === 1) {
+                    arrayOfAvailableMoves.push(availableSquare);
+                }
+            }
+            }
+        }
+        return arrayOfAvailableMoves;
     }
+
 }
