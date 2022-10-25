027869070b07f1bd5d9736ac89e5ed7c08d67ecb
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-25 16:42:54 +0100

    Test 12 - pawh can't move at the edge of the board - completes successfuly

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,44 +11,63 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let arrayOfAvailableMoves = new Array(0);
         const currentSquare = board.findPiece(this);
-
         if (this.player === Player.WHITE) {
+            if (currentSquare.row < 7) {
             let availableSquare = Square.at(
                 currentSquare.row + 1,
                 currentSquare.col
             );
+
             if (this.firstTurn === false) {
                 if (board.isSquareEmpty(availableSquare)) {
-                arrayOfAvailableMoves.push(availableSquare)
+                    arrayOfAvailableMoves.push(availableSquare);
                 }
             } else {
                 let availableSquare2 = Square.at(
                     currentSquare.row + 2,
                     currentSquare.col
                 );
-                if (board.isSquareEmpty(availableSquare) && board.isSquareEmpty(availableSquare2)) {
-                arrayOfAvailableMoves.push(availableSquare, availableSquare2) 
+
+                if (
+                    board.isSquareEmpty(availableSquare) &&
+                    board.isSquareEmpty(availableSquare2)
+                ) {
+                    arrayOfAvailableMoves.push(
+                        availableSquare,
+                        availableSquare2
+                    );
+                }
             }
-            }
-        } else {
+        } 
+    } else {
+        if (currentSquare.row > 0) {
             let availableSquare = Square.at(
                 currentSquare.row - 1,
                 currentSquare.col
             );
+
             if (this.firstTurn === false) {
                 if (board.isSquareEmpty(availableSquare)) {
-                arrayOfAvailableMoves.push(availableSquare)
-            }
+                    arrayOfAvailableMoves.push(availableSquare);
+                }
             } else {
                 let availableSquare2 = Square.at(
                     currentSquare.row - 2,
                     currentSquare.col
                 );
-                if (board.isSquareEmpty(availableSquare) && board.isSquareEmpty(availableSquare2)) {
-                arrayOfAvailableMoves.push(availableSquare, availableSquare2);
+
+                if (
+                    board.isSquareEmpty(availableSquare) &&
+                    board.isSquareEmpty(availableSquare2)
+                ) {
+                    arrayOfAvailableMoves.push(
+                        availableSquare,
+                        availableSquare2
+                    );
                 }
             }
         }
+    }
         return arrayOfAvailableMoves;
     }
 }
