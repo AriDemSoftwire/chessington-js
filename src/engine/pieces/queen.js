6ad32b0d4d5bc57c5937800fb3ccaf9f5321f9fe
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-25 15:39:32 +0100

    Test 11 - Queen can't move through other pieces - completes successfully

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -10,41 +10,80 @@ export default class Queen extends Piece {
     getAvailableMoves(board) {
         let arrayOfAvailableMoves = new Array(0);
         const currentSquare = board.findPiece(this);
-
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
-                let availableSquare = Square.at(i, j);
-                if (
-                    Math.abs(currentSquare.row - availableSquare.row) ===
-                    Math.abs(currentSquare.col - availableSquare.col)
-                ) {
-                    if (
-                        currentSquare.row !== availableSquare.row &&
-                        currentSquare.col !== availableSquare.col
-                    ) {
-                        arrayOfAvailableMoves.push(availableSquare);
+        
+            for (let i = 1; i < 8; i++) {
+                let newSquareUpRight = makeDiagonalSquare(currentSquare.row + i, currentSquare.col + i)
+                if (newSquareUpRight !== undefined) {
+                    if (board.isSquareEmpty(newSquareUpRight) === false) {
+                        break;
                     }
+                    arrayOfAvailableMoves.push(newSquareUpRight);
+                }
+                }
+            for (let i = 1; i < 8; i++) {
+                let newSquareUpLeft = makeDiagonalSquare(currentSquare.row + i, currentSquare.col - i)
+                if (newSquareUpLeft !== undefined) {
+                    if (board.isSquareEmpty(newSquareUpLeft) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareUpLeft);
                 }
             }
-        }
+            for (let i = 1; i < 8; i++) {    
+                let newSquareDownRight = makeDiagonalSquare(currentSquare.row - i, currentSquare.col + i)
+                if (newSquareDownRight !== undefined) {
+                    if (board.isSquareEmpty(newSquareDownRight) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareDownRight);
+                }
+            }
+            for (let i = 1; i < 8; i++) {
+                let newSquareDownLeft = makeDiagonalSquare(currentSquare.row - i, currentSquare.col - i)
+                if (newSquareDownLeft !== undefined) {
+                    if (board.isSquareEmpty(newSquareDownLeft) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareDownLeft);
+                }
+            }
 
+        
         for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
             let availableSquare = Square.at(i, currentSquare.col);
             if (
                 availableSquare.row !== currentSquare.row ||
                 availableSquare.col !== currentSquare.col
             ) {
+                if (board.isSquareEmpty(availableSquare)) {
                 arrayOfAvailableMoves.push(availableSquare);
+                } else {
+                    break;
+                }
             }
-
+        }
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
             let availableSquare2 = Square.at(currentSquare.row, i);
             if (
                 availableSquare2.row !== currentSquare.row ||
                 availableSquare2.col !== currentSquare.col
             ) {
-                arrayOfAvailableMoves.push(availableSquare2);
+                if (board.isSquareEmpty(availableSquare2)) {
+                    arrayOfAvailableMoves.push(availableSquare2);
+                    } else {
+                        break;
+                    }
             }
         }
+
         return arrayOfAvailableMoves;
     }
+
+}
+           
+function makeDiagonalSquare (row, col) {
+            let newSquare = Square.at(row, col);
+            if (newSquare.row >= 0 && newSquare.row <= 7 && newSquare.col >= 0 && newSquare.col <= 7) {
+                return newSquare;
+}
 }
