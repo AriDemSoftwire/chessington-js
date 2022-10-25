3a1aba7f1dd87a38174174614bdf760063d1306e
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-25 11:21:00 +0100

    Queen test runs successfully

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +8,33 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let arrayOfAvailableMoves = new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
+                let availableSquare = Square.at(i, j);
+                if (Math.abs(currentSquare.row - availableSquare.row) === Math.abs(currentSquare.col - availableSquare.col)) {
+                    if (currentSquare.row !== availableSquare.row && currentSquare.col !== availableSquare.col) {
+                        arrayOfAvailableMoves.push(availableSquare);
+                    }
+                }
+               
+            }
+        
+        }
+        
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            let availableSquare = Square.at(i, currentSquare.col);
+            if (availableSquare.row !== currentSquare.row || availableSquare.col !== currentSquare.col) {
+            arrayOfAvailableMoves.push(availableSquare);
+            }
+            
+            let availableSquare2 = Square.at(currentSquare.row, i);
+            if (availableSquare2.row !== currentSquare.row || availableSquare2.col !== currentSquare.col) {
+            arrayOfAvailableMoves.push(availableSquare2);
+            }
+        }
+        return arrayOfAvailableMoves;
     }
 }
