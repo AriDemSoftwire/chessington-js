ab212f7f3e7f79f6a0df081e3a17cabb0c4df603
AriDemSoftwire <arina.demidova@softwire.com>
2022-10-25 15:34:44 +0100

    Test 10: Bishop can't jump over other pieces completes successfully

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,5 +1,4 @@
 import Piece from "./piece";
-import GameSettings from "../gameSettings";
 import Square from "../square";
 
 export default class Bishop extends Piece {
@@ -10,23 +9,52 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         let arrayOfAvailableMoves = new Array(0);
         const currentSquare = board.findPiece(this);
-
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            for (let j = 0; j < GameSettings.BOARD_SIZE; j++) {
-                let availableSquare = Square.at(i, j);
-                if (
-                    Math.abs(currentSquare.row - availableSquare.row) ===
-                    Math.abs(currentSquare.col - availableSquare.col)
-                ) {
-                    if (
-                        currentSquare.row !== availableSquare.row &&
-                        currentSquare.col !== availableSquare.col
-                    ) {
-                        arrayOfAvailableMoves.push(availableSquare);
+        
+            for (let i = 1; i < 8; i++) {
+                let newSquareUpRight = makeDiagonalSquare(currentSquare.row + i, currentSquare.col + i)
+                if (newSquareUpRight !== undefined) {
+                    if (board.isSquareEmpty(newSquareUpRight) === false) {
+                        break;
                     }
+                    arrayOfAvailableMoves.push(newSquareUpRight);
+                }
+                }
+            for (let i = 1; i < 8; i++) {
+                let newSquareUpLeft = makeDiagonalSquare(currentSquare.row + i, currentSquare.col - i)
+                if (newSquareUpLeft !== undefined) {
+                    if (board.isSquareEmpty(newSquareUpLeft) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareUpLeft);
                 }
             }
-        }
+            for (let i = 1; i < 8; i++) {    
+                let newSquareDownRight = makeDiagonalSquare(currentSquare.row - i, currentSquare.col + i)
+                if (newSquareDownRight !== undefined) {
+                    if (board.isSquareEmpty(newSquareDownRight) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareDownRight);
+                }
+            }
+            for (let i = 1; i < 8; i++) {
+                let newSquareDownLeft = makeDiagonalSquare(currentSquare.row - i, currentSquare.col - i)
+                if (newSquareDownLeft !== undefined) {
+                    if (board.isSquareEmpty(newSquareDownLeft) === false) {
+                        break;
+                    }
+                    arrayOfAvailableMoves.push(newSquareDownLeft);
+                }
+            }
         return arrayOfAvailableMoves;
     }
+
 }
+           
+function makeDiagonalSquare (row, col) {
+            let newSquare = Square.at(row, col);
+            if (newSquare.row >= 0 && newSquare.row <= 7 && newSquare.col >= 0 && newSquare.col <= 7) {
+                return newSquare;
+}
+}
+
